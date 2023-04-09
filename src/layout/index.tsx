bf78c2fdb822d93d8d2581e86f82eaa5f2125643
mihilt <kwschic@gmail.com>
2023-04-10 02:23:28 +0900

    feat: add category page properly - put parameters for request in getServerSideProps - add launch.json for vscode debug - fix some styles

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Header from './header';
 
 interface Props {
@@ -8,7 +9,9 @@ export default function Layout({ children }: Props): JSX.Element {
   return (
     <>
       <Header />
-      {children}
+      <Box sx={{ backgroundColor: '#f5f5f5', display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ backgroundColor: '#fff', width: '100%', maxWidth: '1024px' }}>{children}</Box>
+      </Box>
     </>
   );
 }
--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -1,8 +1,12 @@
-import { Box } from '@mui/system';
+import { Box, Divider, Grid, useMediaQuery, useTheme } from '@mui/material';
+
+import moment from 'moment';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
 import { getPostsApi } from '../../apis';
 import Page from '../../components/page';
 import Layout from '../../layout';
+import { isSameDate } from '../../utils';
 
 interface Props {
   count: number;
@@ -12,7 +16,11 @@ interface Props {
 
 export default function Category({ count, posts }: Props): JSX.Element {
   const router = useRouter();
+  const theme = useTheme();
+
   const { category } = router.query;
+
+  const isSmallerThanMd = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <Page>
@@ -20,17 +28,50 @@ export default function Category({ count, posts }: Props): JSX.Element {
         <main>
           <Box>category: {category}</Box>
           <Box>count: {count}</Box>
+          <Divider />
           <Box>
             {posts.map(
               (
                 //TODO: post: Post
                 post: any,
               ) => (
-                <Box key={post.postId}>
-                  <Box>title: {post.title}</Box>
-                  <Box>view: {post.view}</Box>
-                  <Box>like: {post.like}</Box>
-                </Box>
+                <Grid
+                  container
+                  key={post.postId}
+                  sx={{
+                    textAlign: 'center',
+                    fontSize: '12px',
+                    '&:hover': {
+                      backgroundColor: '#f5f5f5',
+                    },
+
+                    borderBottom: '1px solid #e0e0e0',
+                  }}
+                >
+                  <Grid item md={0.5} hidden={isSmallerThanMd}>
+                    {post.categoryId}
+                  </Grid>
+                  <Grid item xs={5.5} md={7} sx={{ textAlign: 'left', cursor: 'pointer' }}>
+                    {post.title}
+                    <span style={{ color: '#000999', fontSize: '10px', fontWeight: 700 }}>
+                      [{post.commentCount}]
+                    </span>
+                  </Grid>
+                  <Grid item xs={3.5} md={2} sx={{ textAlign: 'left' }}>
+                    {post.author}({post.ip})
+                  </Grid>
+                  <Grid item xs={2} md={1.5}>
+                    {isSameDate(new Date(post.createdAt), new Date())
+                      ? moment(post.createdAt).format('HH:mm')
+                      : moment(post.createdAt).format('YYYY-MM-DD')}
+                  </Grid>
+                  <Grid item xs={0.5} md={0.5}>
+                    {post.view}
+                  </Grid>
+                  <Grid item xs={0.5} md={0.5}>
+                    {post.like}
+                  </Grid>
+                </Grid>
               ),
             )}
           </Box>
@@ -40,11 +81,15 @@ export default function Category({ count, posts }: Props): JSX.Element {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await getPostsApi({});
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context: GetServerSidePropsContext,
+) => {
+  const { category } = context.query;
+
+  const res = await getPostsApi({ category: category as string });
   const { count, result: posts } = res.data;
 
   return {
     props: { count, posts },
   };
-}
+};
