3faf439216395f45e503a328b17b7feeae90f8d2
mihilt <kwschic@gmail.com>
2023-04-16 01:20:04 +0900

    fix: fix to use proxy
    - to avoid mixed-content error in vercel

--- next.config.js
+++ next.config.js
@@ -3,8 +3,18 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     API_URL: process.env.API_URL,
+    CLIENT_URL: process.env.CLIENT_URL,
     API_VERSION: process.env.API_VERSION,
     RECOMMENDED_POST_LIKE: 5,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
   },
 };
 
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const requestApi: AxiosInstance = axios.create({
-  baseURL: `${process.env.API_URL}/${process.env.API_VERSION}`,
+  // host 명시적 지정 없이는 getServerSideProps 내에서와 클라이언트에서의 host가 다르다. (localhost:80)
+  baseURL: `${process.env.CLIENT_URL}/api/${process.env.API_VERSION}`,
 });
 
 export default requestApi;
